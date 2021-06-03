c54b327f7fe2e9b14ab4cce15f4be0438f1e4832
ikefollmann <henriquelfollmann@aluno.santoangelo.uri.br>
2021-06-03 16:21:30 -0300

    Testando comando2

--- webwallet/management/commands/teste.py
+++ webwallet/management/commands/teste.py
@@ -1,1 +1,5 @@
-print('Hello, World!!!')
+from django.core.management.base import BaseCommand, CommandError
+
+class Command(BaseCommand):
+    def handle(self, *args, **kwargs):
+        print('Hello, World!!!')
