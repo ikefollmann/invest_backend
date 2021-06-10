df740140bcd6934fc771a3e452466d8ce7c2f897
ikefollmann <henriquelfollmann@aluno.santoangelo.uri.br>
2021-06-09 21:41:57 -0300

    nova correcao

--- webwallet/management/commands/cotacoes_atuais.py
+++ webwallet/management/commands/cotacoes_atuais.py
@@ -2,15 +2,20 @@ from django.core.management.base import BaseCommand
 from django.db.models import Max
 from yahooquery import Ticker
 from webwallet.models import Acao
+from datetime import date
 
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
+        hoje = date.today()
         ult_data = Acao.objects.aggregate(Max('data_cotacao'))
-        acoes = Acao.objects.filter(data_cotacao=ult_data)
-        for acao in acoes:
-            ticker = acao.ticker+'.sa'
-            stock = Ticker(ticker)
-            cotacao = stock.price[ticker]['regularMarketPrice']
 
-            acao.cotacao = float(cotacao)
-            acao.save()
+        if hoje == ult_data:
+            acoes = Acao.objects.filter(data_cotacao=ult_data)
+            for acao in acoes:
+                ticker = acao.ticker+'.sa'
+                stock = Ticker(ticker).price
+
+                if stock[ticker]['marketState'] == 'REGULAR':
+                    cotacao = stock[ticker]['regularMarketPrice']
+                    acao.cotacao = float(cotacao)
+                    acao.save()
