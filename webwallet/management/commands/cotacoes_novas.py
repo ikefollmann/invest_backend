5826acfa2ab4c5d781c157d683161805bd8af9dd
ikefollmann <henriquelfollmann@aluno.santoangelo.uri.br>
2021-06-13 01:18:48 -0300

    denovo

--- webwallet/management/commands/cotacoes_novas.py
+++ webwallet/management/commands/cotacoes_novas.py
@@ -6,16 +6,16 @@ from datetime import datetime
 
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
-    tickers = Acao.objects.distinct().values('ticker')
-    ult_data = Acao.objects.aggregate(Max('data_cotacao'))
-    for ticker in tickers:
-        nome = ticker['ticker']+'.sa'
-        stock = Ticker(nome).price
-        if stock[nome]['marketState'] == 'REGULAR' and stock[nome]['regularMarketTime'][0:10] != str(ult_data):
-            hoje = stock[nome]['regularMartketTime'][0:10]
-            cotacao = stock[nome]['regularMarketPrice']
-            acao = Acao(ticker=ticker, data_cotacao=hoje, cotacao=cotacao)
-            acao.save()
-        else:
-            #print('Mercado aberto:'+str(stock[nome]['marketState'] == 'REGULAR'))
-            print('Ultima data:'+str(stock[nome]['regularMarketTime'][0:10] != str(ult_data)))
+        tickers = Acao.objects.distinct().values('ticker')
+        ult_data = Acao.objects.aggregate(Max('data_cotacao'))
+        for ticker in tickers:
+            nome = ticker['ticker']+'.sa'
+            stock = Ticker(nome).price
+            if stock[nome]['marketState'] == 'REGULAR' and stock[nome]['regularMarketTime'][0:10] != str(ult_data):
+                hoje = stock[nome]['regularMartketTime'][0:10]
+                cotacao = stock[nome]['regularMarketPrice']
+                acao = Acao(ticker=ticker, data_cotacao=hoje, cotacao=cotacao)
+                acao.save()
+            else:
+                #print('Mercado aberto:'+str(stock[nome]['marketState'] == 'REGULAR'))
+                print('Ultima data:'+str(stock[nome]['regularMarketTime'][0:10] != str(ult_data)))
