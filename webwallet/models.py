de1891618b2ce5089f2c0d44522645bc3461dcaa
ikefollmann <henriquelfollmann@aluno.santoangelo.uri.br>
2021-06-14 18:34:18 -0300

    default

--- webwallet/models.py
+++ webwallet/models.py
@@ -36,7 +36,7 @@ class Ativo(models.Model):
     data_compra = models.DateField()
     preco_compra = models.DecimalField(max_digits=12, decimal_places=2)
     carteira = models.ForeignKey(Carteira, on_delete=models.CASCADE, db_index=True)
-    cotas = models.BigIntegerField()
+    cotas = models.PositiveBigIntegerField(default=1)
 
 
 class Relatorio(models.Model):
