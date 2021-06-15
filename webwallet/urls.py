5c93832f29048372a82d3de20652858587653184
Kelvin <kelvinnicolau7@gmail.com>
2021-06-15 19:55:11 -0300

    Att url

--- webwallet/urls.py
+++ webwallet/urls.py
@@ -4,5 +4,4 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('register', views.register, name='register'),
-    path('index', views.index, name='index'),
 ]
