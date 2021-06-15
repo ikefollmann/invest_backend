3fca30fe8dc72c03200aef62c6b445f0cef24e8c
Kelvin <kelvinnicolau7@gmail.com>
2021-06-15 19:28:42 -0300

    Teste Front 1

--- webwallet/urls.py
+++ webwallet/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
-
 from . import views
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('register', views.register, name='register'),
     path('index', views.index, name='index'),
 ]
+
+urlpatterns = patterns('',
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$')
+
+)
