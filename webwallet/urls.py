a1913b1c75d7783ab6022b0f021c30c68dc1bc09
Kelvin <kelvinnicolau7@gmail.com>
2021-06-15 19:30:48 -0300

    Att urls 2

--- webwallet/urls.py
+++ webwallet/urls.py
@@ -1,7 +1,5 @@
 from django.urls import path
 from . import views
-from django.conf.urls import patterns, include, url
-from django.contrib import admin
 
 urlpatterns = [
     path('', views.index, name='index'),
