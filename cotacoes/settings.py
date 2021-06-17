c798649fb9d8bd73fc02e76c295c1eb0f3b4831d
cristian.andrzejewski <cristian.andrzejewski@lojasbecker.com>
2021-06-17 14:56:11 -0300

    teste

--- cotacoes/settings.py
+++ cotacoes/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'sslserver',
     'webwallet.apps.WebwalletConfig',
+    'apps.users'
 ]
 
 MIDDLEWARE = [
--- webwallet/views.py
+++ webwallet/views.py
@@ -1,46 +1,82 @@
+# from django.shortcuts import render
+# from django.http import HttpResponse
+# from rest_framework import viewsets, permissions
+# from django.contrib.auth.models import User, Group
+# from webwallet.serializers import UserSerializer, GroupSerializer
+# from django.contrib.auth.forms import UserCreationForm
+
+
+
+# # Create your views here.
+
+# class UserViewSet(viewsets.ModelViewSet):
+#     queryset = User.objects.all().order_by('-date_joined')
+#     serializer_class = UserSerializer
+#     permission_classes = [permissions.IsAuthenticated]
+
+
+# class GroupViewSet(viewsets.ModelViewSet):
+#     queryset = Group.objects.all()
+#     serializer_class = GroupSerializer
+#     permission_classes = [permissions.IsAuthenticated]
+
+# def index(request):
+#     return render(request = request, template_name = 'index.html')
+
+# def blog(request):
+#     return render(request = request, template_name = 'blog.html')
+
+# def ajuda(request):
+#     return render(request = request, template_name = 'ajuda.html')
+
+# def blogdetalhe(request):
+#     return render(request = request, template_name = 'blog-detalhe.html')
+
+# def cadastro(request):
+#     return render(request = request, template_name = 'cadastro.html')
+
+# def login(request):
+#     return render(request = request, template_name = 'login.html')
+
+# def carteira(request):
+#     return render(request = request, template_name = 'carteira.html')
+
+# def relatorio(request):
+#     return render(request = request, template_name = 'relatorio.html')
+
+# def conta(request):
+#     return render(request = request, template_name = 'conta.html')
+
+
 from django.shortcuts import render
-from django.http import HttpResponse
-from rest_framework import viewsets, permissions
-from django.contrib.auth.models import User, Group
-from webwallet.serializers import UserSerializer, GroupSerializer
-from django.contrib.auth.forms import UserCreationForm
+from django.views.generic import CreateView
+from django.http import HttpResponseRedirect
+from django.contrib.auth.views import login
+from django.contrib.auth.views import logout
+from django.core.urlresolvers import reverse_lazy, reverse
 
-# Create your views here.
-
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all().order_by('-date_joined')
-    serializer_class = UserSerializer
-    permission_classes = [permissions.IsAuthenticated]
+from .forms import CustomUserCreationForm
 
 
-class GroupViewSet(viewsets.ModelViewSet):
-    queryset = Group.objects.all()
-    serializer_class = GroupSerializer
-    permission_classes = [permissions.IsAuthenticated]
+def home(request):
+    return render(request, 'users/home.html')
 
-def index(request):
-    return render(request = request, template_name = 'index.html')
 
-def blog(request):
-    return render(request = request, template_name = 'blog.html')
+def login_view(request, *args, **kwargs):
+    if request.user.is_authenticated():
+        return HttpResponseRedirect(reverse('users:home'))
 
-def ajuda(request):
-    return render(request = request, template_name = 'ajuda.html')
+    kwargs['extra_context'] = {'next': reverse('users:home')}
+    kwargs['template_name'] = 'users/login.html'
+    return login(request, *args, **kwargs)
 
-def blogdetalhe(request):
-    return render(request = request, template_name = 'blog-detalhe.html')
 
-def cadastro(request):
-    return render(request = request, template_name = 'cadastro.html')
+def logout_view(request, *args, **kwargs):
+    kwargs['next_page'] = reverse('users:home')
+    return logout(request, *args, **kwargs)
 
-def login(request):
-    return render(request = request, template_name = 'login.html')
 
-def carteira(request):
-    return render(request = request, template_name = 'carteira.html')
-
-def relatorio(request):
-    return render(request = request, template_name = 'relatorio.html')
-
-def conta(request):
-    return render(request = request, template_name = 'conta.html')
+class RegistrationView(CreateView):
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy('users:login')
+    template_name = "users/register.html"
