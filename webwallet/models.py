73ffd7f9ace3d33e6985057b833c8ef7d1ea62b0
cristian.andrzejewski <cristian.andrzejewski@lojasbecker.com>
2021-06-17 14:39:15 -0300

    teste

--- webwallet/models.py
+++ webwallet/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.translation import ugettext as _
+from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import PermissionsMixin
 
 # Create your models here.
 #teste
@@ -68,10 +71,52 @@ class Relatorio(models.Model):
         a+= ']}'
         return a
 
-class Cadastro(models.Model):
+# class Cadastro(models.Model):
     
-    nome = models.CharField(max_length=255, null=False)
-    email = models.CharField(max_length=255, null=False)
-    telefone = models.CharField(max_length=15, null=False)
-    senha = models.CharField(max_length=100, null=False)
-    confirma_senha = models.CharField(max_length=100, null=False)
+#     nome = models.CharField(max_length=255, null=False)
+#     email = models.CharField(max_length=255, null=False)
+#     telefone = models.CharField(max_length=15, null=False)
+#     senha = models.CharField(max_length=100, null=False)
+#     confirma_senha = models.CharField(max_length=100, null=False)
+
+class EmailUserManager(BaseUserManager):
+    def create_user(self, *args, **kwargs):
+        email = kwargs["email"]
+        email = self.normalize_email(email)
+        password = kwargs["password"]
+        kwargs.pop("password")
+
+        if not email:
+            raise ValueError(_('Users must have an email address'))
+
+        user = self.model(**kwargs)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, *args, **kwargs):
+        user = self.create_user(**kwargs)
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+
+class MyUser(PermissionsMixin, AbstractBaseUser):
+    email = models.EmailField(
+        verbose_name=_('Email address'),
+        unique=True,
+    )
+    first_name = models.CharField(
+        verbose_name=_('Nome'),
+        max_length=50,
+        blank=False,
+        help_text=_('Inform your name'),
+    )
+    last_name = models.CharField(
+        verbose_name=_('Sobrenome'),
+        max_length=50,
+        blank=False,
+        help_text=_('Inform your last name'),
+    )
+    USERNAME_FIELD = 'email'
+    objects = EmailUserManager()
