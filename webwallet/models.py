78bfbdd70bea5762e373f2f1e3aa5c3368be0380
cristian.andrzejewski <cristian.andrzejewski@lojasbecker.com>
2021-06-17 14:09:07 -0300

    .

--- webwallet/models.py
+++ webwallet/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-
+#teste
 class Carteira(models.Model):
     nome = models.CharField(max_length=100)
     cliente = models.ForeignKey(User, on_delete=models.CASCADE)
